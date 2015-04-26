107c95deff7a119af463e0009c415dc27bd60bd9
Mizuki <uta.mizuki@gmail.com>
2015-04-26 16:33:15 +0900

    first commit (only rails new)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GardenMemo::Application.config.secret_key_base = '07cafd8d109a40f1905531fe1a9bc8893551f2cf7f6d40284318cfeef87c6d7225c16bca9f4486ffd54d68e0e7d4ed4ebf05195fe99dff62ed1a90b66fed42ef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GardenMemo::Application.config.session_store :cookie_store, key: '_garden_memo_session'
